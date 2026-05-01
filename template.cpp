f34a4c92616837ed4a3455fedcd4c47b30fae1de
Ratchanon Hsu <133627032+kharryhsu@users.noreply.github.com>
2026-05-01 15:24:10 +0800

    ADD: add coding template

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast IO 
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+
+// Solve Function
+void solve() {
+    int n;
+    cin >> n;
+}
+
+// Main Function
+int main() {
+    fastio;
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
